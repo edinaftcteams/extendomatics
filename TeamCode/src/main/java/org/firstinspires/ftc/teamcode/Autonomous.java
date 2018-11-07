5cfd44459871b36d7357ba78c12d57daf1cffabf
Taicui <43393266+Taicui@users.noreply.github.com>
2018-11-06 18:10:54 -0600

    Update TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -47,7 +47,7 @@ public class Autonomous extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     RobotHardwareClassThingmabobba robot   = new RobotHardwareClassThingmabobba();
     BNO055IMU gyro;
-    static final double COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double COUNTS_PER_MOTOR_REV    = 1120 ;    // eg: TETRIX Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
     static final double     WHEEL_DIAMETER_INCHES   = 3.6 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
